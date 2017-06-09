7bad8437ab1e39f4f2da724a6187e64fd4248c78
ManavGupta04 <manavgupta@hotmail.co.uk>
2017-06-09 20:06:15 +0100

    Added maps activity and buttons on mainActivity

--- volunteer/Volunteer/app/src/main/java/rubicon/volunteer/MainActivity.java
+++ volunteer/Volunteer/app/src/main/java/rubicon/volunteer/MainActivity.java
@@ -1,7 +1,9 @@
 package rubicon.volunteer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void goToMaps(View view){
+        startActivity(new Intent(this, MapsActivity.class));
+    }
+
+    public void goToPReg(View view){
+        startActivity(new Intent(this, PersonalRegActivity.class));
     }
 }
