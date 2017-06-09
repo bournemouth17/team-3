67d11e71c8721b881b6b20c7d27c2ae093976433
Atul <kkmonlee@gmail.com>
2017-06-09 22:54:39 +0100

    Added home page

--- admin/login.php
+++ admin/login.php
@@ -49,3 +49,83 @@
 		}
 	 }
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Coding Cage - Login & Registration System</title>
+<link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"  />
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+
+<div class="container">
+
+ <div id="login-form">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
+
+     <div class="col-md-12">
+
+         <div class="form-group">
+             <h2 class="">Sign In.</h2>
+            </div>
+
+         <div class="form-group">
+             <hr />
+            </div>
+
+            <?php
+   if ( isset($errMSG) ) {
+
+    ?>
+    <div class="form-group">
+             <div class="alert alert-danger">
+    <span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
+                </div>
+             </div>
+                <?php
+   }
+   ?>
+
+            <div class="form-group">
+             <div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+             <input type="text" name="name" class="form-control" placeholder="Username: " value="<?php echo $name; ?>" maxlength="40" />
+                </div>
+                <span class="text-danger"><?php echo $emailError; ?></span>
+            </div>
+
+            <div class="form-group">
+             <div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+             <input type="password" name="pass" class="form-control" placeholder="Password: " maxlength="15" />
+                </div>
+                <span class="text-danger"><?php echo $passError; ?></span>
+            </div>
+
+            <div class="form-group">
+             <hr />
+            </div>
+
+            <div class="form-group">
+             <button type="submit" class="btn btn-block btn-primary" name="btn-login">Sign In</button>
+            </div>
+
+            <div class="form-group">
+             <hr />
+            </div>
+
+            <div class="form-group">
+             <a href="register.php">Sign Up Here...</a>
+            </div>
+
+        </div>
+
+    </form>
+    </div>
+
+</div>
+
+</body>
+</html>
+<?php ob_end_flush(); ?>
