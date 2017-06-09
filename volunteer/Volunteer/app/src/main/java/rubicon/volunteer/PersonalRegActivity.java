6208a0efc8dbacb9271878c3722bdca1d28dca0f
SeanJackson1998 <skatts123@gmail.com>
2017-06-09 21:55:06 +0100

    Volunteer Personal Data Form

--- volunteer/Volunteer/app/src/main/java/rubicon/volunteer/PersonalRegActivity.java
+++ volunteer/Volunteer/app/src/main/java/rubicon/volunteer/PersonalRegActivity.java
@@ -2,8 +2,14 @@ package rubicon.volunteer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class PersonalRegActivity extends AppCompatActivity {
+public class PersonalRegActivity extends AppCompatActivity{
+
+    RadioGroup radioGenderGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
