8124d33f95081bde3f1155030e8d31468ae2ca66
ManavGupta04 <manavgupta@hotmail.co.uk>
2017-06-10 06:21:17 +0100

    Added arduino RFID

---
+++ armband/auth_read_write/processing_pde/Main.java
@@ -0,0 +1,90 @@
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by aa on 25/03/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+        BufferedReader br = null;
+        String address = "/Users/Manav/Documents/codeforgood_part2/team-3/armband/auth_read_write/processing_pde/id.txt";
+        String value = "";
+        try {
+            br = new BufferedReader(new FileReader(address));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            value = sb.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                assert br != null;
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            String link = "http://kkmonlee.com/codeforgood/checkout.php?id=" + value;
+            String results = doHttpUrlConnectionAction(link);
+            System.out.println(results);
+            
+           PrintWriter writer = new PrintWriter("/Users/Manav/Documents/codeforgood_part2/team-3/armband/auth_read_write/processing_pde/id.txt", "UTF-8");
+           writer.println("");
+           writer.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String doHttpUrlConnectionAction(String desired) throws Exception {
+        URL url = null;
+        BufferedReader reader = null;
+        StringBuilder sb;
+
+        try {
+            url = new URL(desired);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+             connection.addRequestProperty("User-Agent", "Mozilla/4.76"); 
+
+            connection.setRequestMethod("GET");
+
+            connection.setReadTimeout(15*1000);
+            connection.connect();
+
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            sb = new StringBuilder();
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ armband/auth_read_write/processing_pde/processing_pde.pde
@@ -0,0 +1,74 @@
+import processing.net.*;
+
+import processing.serial.*;
+import java.net.*;
+import java.io.*;
+
+Serial myPort;
+String rfidID;
+String wholeString;
+
+void setup() {
+  String portName = Serial.list()[1];
+  printArray(Serial.list());
+
+  myPort = new Serial(this, portName, 115200);
+  size(200, 200);
+  background(0);
+}
+
+void draw() {
+  // Value changed here
+  if (myPort.available() > 0) {
+    rfidID = myPort.readStringUntil('\n');
+  }
+  myPort.write("\n");
+  if(true){
+    try {
+      println("qwertyuiop" + rfidID);
+      PrintWriter writer = new PrintWriter("/Users/Manav/Documents/codeforgood_part2/team-3/armband/auth_read_write/processing_pde/id.txt", "UTF-8");
+      writer.println(rfidID);
+      writer.close();
+      
+      
+    } catch (UnsupportedEncodingException e) {
+      println("UEE");
+    } catch (IOException e) {
+      println("IO");
+    } catch(NumberFormatException e) {
+      println("No");
+    } finally {
+        println("works!");
+      }
+      
+      
+    
+    // Runs Java program to push the changed text file to the website
+    try {
+  //    run("javac Main.java");
+      run("java -classpath /Users/Manav/Documents/codeforgood_part2/team-3/armband/auth_read_write/processing_pde  Main");
+      
+    } catch (Exception e) {
+      println("Exception");
+    }
+  }
+  
+  delay(1000);
+
+}
+
+void printLines(String name, InputStream ins) throws Exception {
+  String line = null;
+  BufferedReader in = new BufferedReader(new InputStreamReader(ins));
+  while ((line = in.readLine()) != null) {
+    System.out.println(name + " " + line);
+  }
+}
+
+void run(String command) throws Exception {
+  Process pro = Runtime.getRuntime().exec(command);
+  printLines(command + " stdout:", pro.getInputStream());
+  printLines(command + " stderr:", pro.getErrorStream());
+  pro.waitFor();
+  println(command + " exitValue() " + pro.exitValue());
+}
